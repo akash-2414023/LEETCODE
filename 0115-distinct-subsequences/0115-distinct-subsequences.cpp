4f5ed2e33970265760b56ccc93cf530ef52704bf
akash-2414023 <mahatoakash2004@gmail.com>
2026-09-06 09:38:46 +0530

    Time: 27 ms (71.17%), Space: 44.2 MB (38.48%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
-    int solve(string& s, string& t, int n, int m, vector<vector<int>>& dp) {
-        if (n == 0 && m != 0)
-            return 0;
-
-        if (m == 0) {
-            return 1;
-        }
-        if (dp[n][m] != -1) {
-            return dp[n][m];
-        }
-        if (s[n - 1] == t[m - 1]) {
-            int take = solve(s , t , n - 1 , m - 1 , dp);
-            int notTake = solve(s, t, n - 1, m, dp);
-            return dp[n][m] = take + notTake;
-        }
-        return dp[n][m] = solve(s, t, n - 1, m, dp);
-    }
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-        return solve(s, t, n, m, dp);
+
+        vector<vector<unsigned long long>> dp(
+            n + 1, vector<unsigned long long>(m + 1, 0)
+        );
+
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+
+                if (s[i - 1] == t[j - 1]) {
+                    unsigned long long take = dp[i - 1][j - 1];
+                    unsigned long long notTake = dp[i - 1][j];
+
+                    dp[i][j] = take + notTake;
+                }
+                else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 };
